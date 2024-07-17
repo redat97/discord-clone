cf3ae62086ba2f21e75b62589345e3b96539bd10
redat97 <tarzaltreda@gmail.com>
2024-07-17 17:08:07 -0400

    finished invitation link start working on making invite URL work - timestamp: 3h55

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -19,23 +19,50 @@ import { useModal } from "@/hooks/use-modal-store";
 import { Label } from "@radix-ui/react-label";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
-import { Copy, RefreshCw } from "lucide-react";
+import { Check, Copy, RefreshCw } from "lucide-react";
 import { useOrigin } from "@/hooks/use-origins";
+import { useState } from "react";
 
 export const InviteModal = () => {
-    const { isOpen, onClose, type, data} = useModal();
+    const { onOpen, isOpen, onClose, type, data } = useModal();
     const origin = useOrigin();
 
     const isModalOpen = isOpen && type === "invite";
     const { server } = data;
-    const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
+
+    const [copied, setCopied] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const inviteUrl = `${origin}/invite-code/${server?.inviteCode}`;
+
+    const onCopy = () => {
+        navigator.clipboard.writeText(inviteUrl);
+        setCopied(true)
+
+        setTimeout(() => {
+            setCopied(false);
+        }, 1000);
+    }
+
+    const onNew = async () => {
+        try {
+            setIsLoading(true);
+            const response = await axios.patch(`/api/servers/${server?.id}/invite-code`)
+
+            onOpen("invite", { server: response.data });
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
 
     return (
         <Dialog open={isModalOpen} onOpenChange={onClose}>
             <DialogContent className="bg-white text-black p-0 overflow-hidden">
                 <DialogHeader className="pt-8 px-6">
                     <DialogTitle className="text-2xl text-center font-bold">
-                        Inviten friends
+                        Invite friends
                     </DialogTitle>
                 </DialogHeader>
                 <div className="p-6">
@@ -44,20 +71,27 @@ export const InviteModal = () => {
                     </Label>
                     <div className="flex items-center mt-2 gap-x-2">
                         <Input
+                            disabled={isLoading}
                             className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
                             value={inviteUrl}
                         />
-                        <Button size="icon">
-                            <Copy className="w-4 h-4" />
+                        <Button disabled={isLoading} onClick={onCopy} size="icon">
+
+                            {copied
+                                ? <Check className="w-4 h-4" />
+                                : <Copy className="w-4 h-4" />
+                            }
                         </Button>
                     </div>
                     <Button
-                    variant="link"
-                    size="sm"
-                    className="text-xs text-zinc-500 mt-4"
+                        onClick={onNew}
+                        disabled={isLoading}
+                        variant="link"
+                        size="sm"
+                        className="text-xs text-zinc-500 mt-4"
                     >
                         Generate a new link
-                        <RefreshCw className="w-4 h-4 ml-2"/>
+                        <RefreshCw className="w-4 h-4 ml-2" />
                     </Button>
                 </div>
             </DialogContent>
